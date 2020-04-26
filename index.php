179045283c2319e1a526dbc5ae2d99001335270f
Achez <achez@inbox.ru>
2020-04-26 09:58:54 +0300

    Исправлено неверное имя массива

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ $goods=[
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-			<?php foreach($cat as $key => $val): ?>
+			<?php foreach($cats as $key => $val): ?>
             <li class="nav__item">
                 <a href="pages/all-lots.html"><?= $val ?></a>
             </li>
