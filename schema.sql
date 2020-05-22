596177f3d77358140ab95d45b8bf3bb68ada40cb
Achez <achez@inbox.ru>
2020-05-22 20:27:09 +0300

    Добавлены индексы и пропущенные поля

--- schema.sql
+++ schema.sql
@@ -18,8 +18,8 @@ CREATE TABLE lot
   name VARCHAR(255),
   description VARCHAR(1000),
   image VARCHAR(255),
-  start_price NUMERIC(12,2),
-  expiration_date DATETIME,
+  start_price NUMERIC(12,2) NOT NULL,
+  expiration_date DATETIME NOT NULL,
   bid_step NUMERIC(6,2),
   author_id int NOT NULL,
   winner_id int NOT NULL,
@@ -31,7 +31,8 @@ CREATE TABLE bid
   id int PRIMARY KEY auto_increment,
   bid_date DATETIME,
   bid_amount NUMERIC(12,2),
-  user_id int NOT NULL
+  user_id int NOT NULL,
+  lot_id int NOT NULL
 );
 
 CREATE TABLE t_user
@@ -43,3 +44,15 @@ CREATE TABLE t_user
   password CHAR(60),
   contacts VARCHAR(1000)
 );
+
+CREATE INDEX i_lot_by_date ON lot(creation_date);
+CREATE INDEX i_lot_by_exp ON lot(expiration_date);
+CREATE INDEX i_lot_by_name ON lot(name);
+CREATE INDEX i_lot_by_author ON lot(author_id);
+CREATE INDEX i_lot_by_winner ON lot(winner_id);
+CREATE INDEX i_lot_by_category ON lot(category_id);
+CREATE INDEX i_bid_by_date ON bid(bid_date);
+CREATE INDEX i_bid_by_bidder ON bid(user_id);
+CREATE INDEX i_bid_by_lot ON bid(lot_id);
+CREATE INDEX i_user_by_email ON t_user(email);
+CREATE INDEX i_user_by_name ON t_user(name);
