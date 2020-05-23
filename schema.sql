06e87d22a5e5b5eb3e0e5ac45eeee7c0cb648947
Achez <achez@inbox.ru>
2020-05-23 11:44:36 +0300

    куча косметики

--- schema.sql
+++ schema.sql
@@ -1,23 +1,18 @@
 CREATE DATABASE yeticave
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_general_ci;
   
 USE yeticave;
 
-/* CREATE TABLE category
+/*
+CREATE TABLE category
 (
   id int PRIMARY KEY auto_increment,
   name VARCHAR(255),
   code VARCHAR(30),
 );
- */
- CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `code` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/* CREATE TABLE lot
+
+CREATE TABLE lot
 (
   id int PRIMARY KEY auto_increment,
   creation_date DATETIME,
@@ -30,17 +25,45 @@ USE yeticave;
   author_id int NOT NULL,
   winner_id int NOT NULL,
   category_id int NOT NULL
-  );
- */
+);
+
+CREATE TABLE bid
+(
+  id int PRIMARY KEY auto_increment,
+  bid_date DATETIME,
+  bid_amount DECIMAL(12,2),
+  user_id int NOT NULL,
+  lot_id int NOT NULL
+);
+
+CREATE TABLE user
+(
+  id int PRIMARY KEY auto_increment,
+  reg_date DATETIME,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255) NOT NULL,
+  password CHAR(60) NOT NULL,
+  contacts VARCHAR(1000) NOT NULL
+);
+*/
+
+ CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cat_by_code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
  CREATE TABLE `lot` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `creation_date` datetime DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `creation_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(255) NOT NULL,
   `description` varchar(1000) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `start_price` decimal(12,2) NOT NULL,
   `expiration_date` datetime NOT NULL,
-  `bid_step` decimal(6,2) DEFAULT NULL,
+  `bid_step` decimal(6,2) NOT NULL,
   `author_id` int(11) NOT NULL,
   `winner_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
@@ -51,47 +74,27 @@ USE yeticave;
   KEY `lot_by_author` (`author_id`),
   KEY `lot_by_winner` (`winner_id`),
   KEY `lot_by_category` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
  
-/* CREATE TABLE bid
-(
-  id int PRIMARY KEY auto_increment,
-  bid_date DATETIME,
-  bid_amount DECIMAL(12,2),
-  user_id int NOT NULL,
-  lot_id int NOT NULL
-);
- */
 CREATE TABLE `bid` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `bid_date` datetime DEFAULT NULL,
-  `bid_amount` decimal(12,2) DEFAULT NULL,
+  `bid_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `bid_amount` decimal(12,2) NOT NULL,
   `user_id` int(11) NOT NULL,
   `lot_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `bid_by_date` (`bid_date`),
   KEY `bid_by_bidder` (`user_id`),
   KEY `bid_by_lot` (`lot_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/* CREATE TABLE user */
-/* ( */
-  /* id int PRIMARY KEY auto_increment, */
-  /* reg_date DATETIME, */
-  /* email VARCHAR(255) NOT NULL UNIQUE, */
-  /* name VARCHAR(255) NOT NULL, */
-  /* password CHAR(60) NOT NULL, */
-  /* contacts VARCHAR(1000) NOT NULL */
-/* ); */
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `reg_date` datetime DEFAULT NULL,
+  `reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `email` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `password` varchar(60) DEFAULT NULL,
-  `contacts` varchar(1000) DEFAULT NULL,
+  `password` varchar(60) NOT NULL,
+  `contacts` varchar(1000) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
-  KEY `user_by_email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
