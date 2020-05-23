0779bd960eb1725b8b170fa3fb8fe24202bfe9c8
Achez <achez@inbox.ru>
2020-05-23 12:29:00 +0300

    Убраны закомментированные старые объявления

--- schema.sql
+++ schema.sql
@@ -4,49 +4,6 @@ CREATE DATABASE yeticave
   
 USE yeticave;
 
-/*
-CREATE TABLE category
-(
-  id int PRIMARY KEY auto_increment,
-  name VARCHAR(255),
-  code VARCHAR(30),
-);
-
-CREATE TABLE lot
-(
-  id int PRIMARY KEY auto_increment,
-  creation_date DATETIME,
-  name VARCHAR(255),
-  description VARCHAR(1000),
-  image VARCHAR(255),
-  start_price DECIMAL(12,2) NOT NULL,
-  expiration_date DATETIME NOT NULL,
-  bid_step DECIMAL(6,2),
-  author_id int NOT NULL,
-  winner_id int NOT NULL,
-  category_id int NOT NULL
-);
-
-CREATE TABLE bid
-(
-  id int PRIMARY KEY auto_increment,
-  bid_date DATETIME,
-  bid_amount DECIMAL(12,2),
-  user_id int NOT NULL,
-  lot_id int NOT NULL
-);
-
-CREATE TABLE user
-(
-  id int PRIMARY KEY auto_increment,
-  reg_date DATETIME,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255) NOT NULL,
-  password CHAR(60) NOT NULL,
-  contacts VARCHAR(1000) NOT NULL
-);
-*/
-
  CREATE TABLE `category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
