0d607f6fbb8517e232d38e5c3389b57542d7dfdd
Achez <achez@inbox.ru>
2020-05-29 10:22:28 +0300

    Использована функция Coalesce()

--- index.php
+++ index.php
@@ -7,17 +7,12 @@ $user_name = 'Achez'; // укажите здесь ваше имя
 $title='Yeti Cave';
 
 include 'connect.php';
-$sql="SELECT L.name lot_name, start_price, image, C.name cat_name, expiration_date, ";
-$sql=$sql."(SELECT bid_amount FROM bid WHERE lot_id=L.id ORDER BY bid_date DESC LIMIT 1)";
-$sql=$sql."last_bid FROM lot L JOIN category C ON L.category_id = C.id WHERE ";
-$sql=$sql."expiration_date >= CURRENT_TIMESTAMP ORDER BY creation_date DESC  LIMIT 9";
+$sql="SELECT L.name lot_name, start_price, image, C.name cat_name, expiration_date,
+coalesce((SELECT bid_amount FROM bid WHERE lot_id=L.id ORDER BY bid_date DESC LIMIT 1),
+L.start_price) price FROM lot L JOIN category C ON L.category_id = C.id
+WHERE expiration_date >= CURRENT_TIMESTAMP ORDER BY creation_date DESC  LIMIT 9";
 $result = mysqli_query($con, $sql);
 $goods = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-foreach($goods as $num => $row)
-{
-    $row['last_bid'] == NULL ? $goods[$num]['price']=$row['start_price'] : $goods[$num]['price']=$row['last_bid'];
-}
 
 /*заполняем массив $cats*/
 $sql = "SELECT * FROM category";
@@ -26,4 +21,3 @@ $cats = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 $main_text=include_template('main.php', ['cats'=>$cats, 'goods'=>$goods]);
 print include_template('layout.php', ['auth'=>$is_auth, 'user'=>$user_name,'main'=> $main_text, 'title'=>$title, 'cats'=>$cats]);
-
